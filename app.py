75fd2f6e71de257a4d232148f50444caa9c39787
iamhridoy11 <m_rahman_hridoy@yahoo.com>
2023-08-23 14:46:03 -0400

    push

---
+++ app.py
@@ -0,0 +1,47 @@
+from flask import Flask, render_template, redirect, request
+from flask_debugtoolbar import DebugToolbarExtension
+from surveys import satisfaction_survey
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "chickeneater12"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+debug = DebugToolbarExtension(app)
+
+response = []
+
+
+@app.route('/')
+def home_page():
+
+    return render_template('home.html', survey=satisfaction_survey)
+
+
+@app.route('/start', methods=["POST"])
+def show_begain():
+    RESPONSE = []
+    return redirect('/questions/0')
+
+
+@app.route('/questions/<int:id>')
+def show_question(id):
+    question = satisfaction_survey.questions[id]
+
+    return render_template('questions.html', questionNumber=id, question=question)
+
+
+@app.route('/answer', methods=["POST"])
+def work_with_answer():
+
+    choice = request.form['answer']
+    response.append(choice)
+
+    if (len(response) == len(satisfaction_survey.questions)):
+        return redirect('/complete')
+    else:
+        return redirect(f"/questions/{len(response)}")
+
+
+@app.route('/complete')
+def complete_survey():
+    return render_template('complete.html')
