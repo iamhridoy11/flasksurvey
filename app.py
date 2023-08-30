60150c3d93f6cc245c98b165dc8b90e58918dea7
iamhridoy11 <m_rahman_hridoy@yahoo.com>
2023-08-30 13:29:37 -0400

    update session

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-from flask import Flask, render_template, redirect, request
+from flask import Flask, render_template, redirect, request, session, flash
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey
+
+# key names will use to store some things in the session;
+# put here as constants so we're guaranteed to be consistent in
+# our spelling of these
+RESPONSES_KEY = "response"
 
 
 app = Flask(__name__)
@@ -8,7 +13,7 @@ app.config['SECRET_KEY'] = "chickeneater12"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app)
 
-response = []
+# response = []
 
 
 @app.route('/')
@@ -19,27 +24,63 @@ def home_page():
 
 @app.route('/start', methods=["POST"])
 def show_begain():
-    RESPONSE = []
+    """Adding an empty session of response"""
+    session[RESPONSES_KEY] = []
+
+    # RESPONSE = []
+
     return redirect('/questions/0')
 
 
 @app.route('/questions/<int:id>')
 def show_question(id):
+    """Display current question."""
+    responses = session.get(RESPONSES_KEY)
+
+    if (responses is None):
+        # trying to access question page too soon
+        return redirect("/")
+
+    if (len(responses) == len(satisfaction_survey.questions)):
+        # They've answered all the questions! Thank them.
+        return redirect("/complete")
+
+    if (len(responses) != id):
+        # Trying to access questions out of order.
+        flash(f"Invalid question id: {id}.")
+        return redirect(f"/questions/{len(responses)}")
+
     question = satisfaction_survey.questions[id]
 
-    return render_template('questions.html', question=question)
+    return render_template("questions.html", question_num=id, question=question)
+    # question = satisfaction_survey.questions[id]
+
+    # return render_template('questions.html', question=question)
 
 
 @app.route('/answer', methods=["POST"])
 def work_with_answer():
 
     choice = request.form['answer']
-    response.append(choice)
 
-    if (len(response) == len(satisfaction_survey.questions)):
-        return redirect('/complete')
+    # Adding response to the session
+    responses = session[RESPONSES_KEY]
+    responses.append(choice)
+    session[RESPONSES_KEY] = responses
+
+    # response.append(choice)
+
+    if (len(responses) == len(satisfaction_survey.questions)):
+        # They've answered all the questions! Thank them.
+        return redirect("/complete")
+
     else:
-        return redirect(f"/questions/{len(response)}")
+        return redirect(f"/questions/{len(responses)}")
+
+    # if (len(response) == len(satisfaction_survey.questions)):
+    #     return redirect('/complete')
+    # else:
+    #     return redirect(f"/questions/{len(response)}")
 
 
 @app.route('/complete')
